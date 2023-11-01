980977898e3b13daefc8b12b8ca758ac7280e0aa
Philippe Lemaire <philippe.lemaire@protonmail.com>
2023-11-01 08:33:22 +0100

    up to floor 3.3

--- gradientdescent/encounters_per_floor.py
+++ gradientdescent/encounters_per_floor.py
@@ -1,3 +1,6 @@
+from .dice import get_key, roll_d10
+
+
 encounters_per_floor = {
     "1": {
         0: "1d10 Troubleshooters",
@@ -13,5 +16,25 @@ encounters_per_floor = {
         8: "Ghost in the Machine",
         9: "A diver",
     },
-    "3": {},
+    "3.1": {
+        0: "Waste disposal, Save or 1d6 damage",
+        3: "1d10 Fallen Androids",
+        6: "1d10 Security Androids",
+        8: "Ghost in the Machine",
+        9: "A diver",
+    },
+    "3.2": {
+        0: "A diver",
+        3: "2d10 Bee Drones (p38)",
+        6: "1d10 Forgotten Androids",
+        8: "1d10 Security Androids",
+        9: "Ghost in the Machine",
+    },
+    "3.3": {
+        0: "A diver",
+        3: "Silus makes Demands (p40)",
+        6: "1d10 Pseudomilk Eels (p42)",
+        8: "1d5 Stunted Androids (p41)",
+        9: "A Ghost in the Machine",
+    },
 }
