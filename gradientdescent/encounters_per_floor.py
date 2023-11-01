db0a1d36cec320d741212bf25d9fe7a90f518cc4
Philippe Lemaire <philippe.lemaire@protonmail.com>
2023-11-01 08:09:03 +0100

    floor 2 added

--- gradientdescent/encounters_per_floor.py
+++ gradientdescent/encounters_per_floor.py
@@ -5,5 +5,16 @@ encounters_per_floor = {
         6: "Distorted corporate muzak plays over the intercom.",
         8: "A Security Android",
         9: "A diver.",
-    }
+    },
+    2: {
+        0: "The Minautor",
+        3: "1d5 Chosen Androids",
+        6: "1d10 Fallen Androids",
+        8: "Ghost in the Machine",
+        9: "A diver",
+    },
+    3: {},
+    4: {},
+    5: {},
+    6: {},
 }
